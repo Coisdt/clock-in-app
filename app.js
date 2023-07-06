77bf3b3fe9c5d3fb862ee53edb82243b645026b9
Francois <cois25@live.com>
2023-07-07 01:20:32 +0200

    first attempt from way back

---
+++ app.js
@@ -0,0 +1,63 @@
+//#1
+// const teachers = document.querySelector(".login")
+
+// // const specificTeacher = document.getElementById("teachers").teachers;
+
+    
+// if ()) {
+//     "login success"
+// }
+// else {
+// "login fail"
+// }
+
+// getID("Logged IN succesfully");
+
+
+
+
+//#2
+
+
+// const persalID = teachers.find(function (teacher) {
+//   teacher.persalNumber = teacher;
+//   console.log(persalID);
+//   return teacher.persalNumber;
+// });
+
+
+
+//#3
+
+//select element
+// const persalLogin = document.querySelector(".persalLogIn");
+// const logInBtn = document.querySelector(".LogIn-btn");
+
+// logInBtn.addEventListener('click', function(){
+//   persalLogin.find()
+// })
+
+
+//#4
+
+// select element
+
+
+function getdata(){
+const password = document.querySelector(".password");
+const submit = document.querySelector(".submit");
+const status = document.querySelector('.status')
+
+const getPersal = teachers.getItem('persalNumber').value;
+console.log(getPersal);
+
+if(submission.contains(persalNumber.value)){
+  return status.innerHTML = 'successful'
+}
+else{
+  return ('ah shit')
+};
+
+}
+
+getdata();
---
+++ data.js
@@ -0,0 +1,10 @@
+//here all the data of the teachers will appear
+
+
+
+
+const teachers = [
+    {firstName:'francois', surName: 'du Toit', persalNumber: '57083975'},
+    {firstName: 'john', surName: 'fortuin', persalNumber: '12345678'},
+    {firstName: 'HD', surName: 'Potgieter', persalNumber: '87654321'}
+]
